2f4fd944d7870cd0d91fa635da9c7d5fb85b8aab
Sathiya priya <97939755+gsathiya91@users.noreply.github.com>
2022-08-21 18:25:33 +0530

    Add files via upload

---
+++ src/categories.js
@@ -0,0 +1,28 @@
+const categories = [
+    {
+        name: "Fruits",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0uGryHUp0JKvEi3qQQOqdDPICPUyHP6sJrg&usqp=CAU"
+    },
+    {
+        name: "Vegetables",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWfMwxj3-YVVp4BUWbRTaLIkLOpVMrPK8OrQ&usqp=CAU"  
+    },
+    {
+        name: "Spices",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnBbsPX8Fb09ySxqd4xYhkZg05Bg-K4Rs7dw&usqp=CAU"
+    },
+    {
+        name: "Kitchen Items",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb5x6aLTTQsO-5vpjpVVN23tQVtPonhQGoWg&usqp=CAU"
+    },
+    {
+        name: "Health Drinks",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa5ygyN8BRLBJshuBea9xoyXx5GGMQ1J8gXw&usqp=CAU"
+    },
+    {
+        name: "Cleaning",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqdYj_OURcegtNGFmCllKSiCFIbGrlQ_yxSw&usqp=CAU"
+    },
+]
+
+export default categories;
